78c4fad0c69b3b1424dd6fc6f32b4dbe223c9e4b
fanfanda <fanfanda@ict.ac.cn>
2018-08-04 17:36:27 +0800

    nothing

--- nothing.py
+++ nothing.py
@@ -18,7 +18,7 @@ from keras.callbacks import ReduceLROnPlateau
 import sys
 
 
-sns.set(style='white', context='notebook', palette='deep')
+# sns.set(style='white', context='notebook', palette='deep')
 np.random.seed(2)
 
 # Load the data
@@ -317,33 +317,33 @@ legend = ax[1].legend(loc='best', shadow=True)
 
 # Look at confusion matrix 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+# def plot_confusion_matrix(cm, classes,
+#                           normalize=False,
+#                           title='Confusion matrix',
+#                           cmap=plt.cm.Blues):
+#     """
+#     This function prints and plots the confusion matrix.
+#     Normalization can be applied by setting `normalize=True`.
+#     """
+#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#     plt.title(title)
+#     plt.colorbar()
+#     tick_marks = np.arange(len(classes))
+#     plt.xticks(tick_marks, classes, rotation=45)
+#     plt.yticks(tick_marks, classes)
 
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#     if normalize:
+#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+#     thresh = cm.max() / 2.
+#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+#         plt.text(j, i, cm[i, j],
+#                  horizontalalignment="center",
+#                  color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+#     plt.tight_layout()
+#     plt.ylabel('True label')
+#     plt.xlabel('Predicted label')
 
 # Predict the values from the validation dataset
 Y_pred = model.predict(X_val)
@@ -354,7 +354,8 @@ Y_true = np.argmax(Y_val,axis = 1)
 # compute the confusion matrix
 confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
 # plot the confusion matrix
-plot_confusion_matrix(confusion_mtx, classes = range(10)) 
+
+# plot_confusion_matrix(confusion_mtx, classes = range(10)) 
 
 
 # Here we can see that our CNN performs very well on all digits with few errors considering the size of the validation set (4 200 images).
