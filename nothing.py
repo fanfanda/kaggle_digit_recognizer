41013db3135e49635d4bd55fd759dfe00fbc5246
fanfanda <fanfanda@ict.ac.cn>
2018-08-04 17:39:54 +0800

    nothing

--- nothing.py
+++ nothing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
