582ff197356460b527008a7d5ce31c16549e467f
fanfanda <fanfanda@ict.ac.cn>
2018-08-05 16:24:45 +0800

    nothing

--- nothing.py
+++ nothing.py
@@ -17,6 +17,23 @@ from keras.callbacks import ReduceLROnPlateau
 
 import sys
 
+import tensorflow as tf
+ 
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0,2.0,3.0],shape=[3],name='a')
+    b = tf.constant([1.0,2.0,3.0],shape=[3],name='b')
+with tf.device('/gpu:1'):
+    c = a+b
+   
+#注意：allow_soft_placement=True表明：计算设备可自行选择，如果没有这个参数，会报错。
+#因为不是所有的操作都可以被放在GPU上，如果强行将无法放在GPU上的操作指定到GPU上，将会报错。
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,log_device_placement=True))
+#sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess.run(tf.global_variables_initializer())
+print(sess.run(c))
+
+
+
 # sns.set(style='white', context='notebook', palette='deep')
 np.random.seed(2)
 
@@ -113,17 +130,17 @@ batch_size = 86
 # With data augmentation to prevent overfitting (accuracy 0.99286)
 
 datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        featurewise_center = False,  # set input mean to 0 over the dataset
+        samplewise_center = False,  # set each sample mean to 0
+        featurewise_std_normalization = False,  # divide inputs by std of the dataset
+        samplewise_std_normalization = False,  # divide each input by its std
+        zca_whitening = False,  # apply ZCA whitening
+        rotation_range = 10,  # randomly rotate images in the range (degrees, 0 to 180)
         zoom_range = 0.1, # Randomly zoom image 
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=False,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+        width_shift_range = 0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range = 0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip = False,  # randomly flip images
+        vertical_flip = False)  # randomly flip images
 
 
 datagen.fit(X_train)
